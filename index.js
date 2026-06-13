d17690ad65b0edc40a13ab76ffa8bf722393a9bd
Mamun123123123 <almalm90459@gmail.com>
2026-06-13 20:39:46 +0600

    Save Jobs Post in the server using server action

--- index.js
+++ index.js
@@ -1,55 +1,69 @@
 const express = require('express')
-const cors = require('cors')
+const cors = require('cors');
 const app = express()
 const port = 5000
-require('dotenv').config()
+require('dotenv').config();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = process.env.DB_USER
+
+
+const uri = process.env.DB_USER;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    
-    const database = client.db("test")
-    const jobCollection = database.collection("jobs")
-
-    app.get('/jobs', async(req,res)=>{
-      
-      const job = req.body;
-      const result = await jobCollection.insertOne(job)
-      res.send(result)
-      
-    })
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
 
+        const database = client.db("hireloop_db");
+        const jobCollection = database.collection("jobs");
+        const companyCollection = database.collection("companies");
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        app.get('/api/jobs', async(req, res) =>{
+            const query = {};
+            if(req.query.companyId){
+                query.companyId = req.query.companyId;
+            }
+            if(req.query.status){
+                query.status = req.query.status;
+            }
+            const cursor = jobCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.post('/api/jobs', async (req, res) => {
+            const job = req.body;
+            const result = await jobCollection.insertOne(job);
+            res.send(result);
+        })
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
@@ -57,9 +71,6 @@ run().catch(console.dir);
 
 
 
-
-
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
