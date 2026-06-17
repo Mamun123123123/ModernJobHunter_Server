6aa6cbfdcbe69e80d58475fa2a1b0f9f02eb0987
Mamun123123123 <almalm90459@gmail.com>
2026-06-17 09:15:29 +0600

    Application api create and successfully connect with database

--- index.js
+++ index.js
@@ -37,9 +37,12 @@ async function run() {
         const jobCollection = database.collection("jobs");
         const companyCollection = database.collection("companies");
         const usersCollection = database.collection("user");
+        const applicationsCollection = database.collection("applications");
+        const planCollection = database.collection('plans');
+        const subscriptionCollection = database.collection('subscriptions');
 
         app.get('/api/users', async (req, res) => {
-            
+
             const cursor = usersCollection.find().skip(6);
             const result = await cursor.toArray();
             res.send(result);
@@ -77,8 +80,31 @@ async function run() {
             res.send(result);
         })
 
-        // company related apis
+        // application related apis
+        app.get('/api/applications', async (req, res) => {
+            const query = {};
+            if (req.query.applicantId) {
+                query.applicantId = req.query.applicantId;
+            }
+            if (req.query.jobId) {
+                query.jobId = req.query.jobId;
+            }
+            const cursor = applicationsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
+        app.post('/api/applications', async (req, res) => {
+            const application = req.body;
+            const newApplication = {
+                ...application,
+                createdAt: new Date()
+            }
+            const result = await applicationsCollection.insertOne(newApplication);
+            res.send(result);
+        })
+
+        // company related apis
         app.get('/api/companies', async (req, res) => {
             const cursor = companyCollection.find().skip(4);
             const result = await cursor.toArray();
@@ -106,6 +132,38 @@ async function run() {
             res.send(result);
         })
 
+        // plans 
+        app.get('/api/plans', async (req, res) => {
+            const query = {}
+            if (req.query.plan_id) {
+                query.id = req.query.plan_id
+            }
+            const plan = await planCollection.findOne(query);
+            res.send(plan)
+        })
+
+        // subscription 
+        app.post('/api/subscriptions', async (req, res) => {
+            const data = req.body;
+            const subsInfo = {
+                ...data,
+                createdAt: new Date()
+            }
+
+            const result = await subscriptionCollection.insertOne(subsInfo);
+
+            // update the user plan information
+            const filter = { email: data.email };
+            // update the value of the 'quantity' field to 5
+            const updateDocument = {
+                $set: {
+                    plan: data.planId,
+                },
+            };
+
+            const updateResult = await usersCollection.updateOne(filter, updateDocument);
+            res.send(updateResult)
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
